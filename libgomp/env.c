a3d12bc0cc655d435c131532576f62f84fac4edf
kyukhin <kyukhin@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-06-04 16:44:15 +0400

    GOMP: Implement omp_is_initial_device.

--- libgomp/env.c
+++ libgomp/env.c
@@ -1418,12 +1418,19 @@ omp_get_team_num (void)
   return 0;
 }
 
+#ifdef ACCEL_COMPILER
 int
 omp_is_initial_device (void)
 {
-  /* Hardcoded to 1 on host, should be 0 on MIC, HSAIL, PTX.  */
+  return 0;
+}
+#else // ACCEL_COMPILER
+int
+omp_is_initial_device (void)
+{
   return 1;
 }
+#endif // ACCEL_COMPILER
 
 ialias (omp_set_dynamic)
 ialias (omp_set_nested)
